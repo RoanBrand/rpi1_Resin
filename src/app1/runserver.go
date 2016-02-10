01dcd85d12ea252c0c5b591b6e03bd614aac176e
Roan Brand <roan.brand@imqs.co.za>
2016-02-10 15:14:53 +0200

    add copy for leaftut

--- src/app1/runserver.go
+++ src/app1/runserver.go
@@ -4,25 +4,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
 )
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()       // parse arguments, you have to call this by yourself
-	fmt.Println(r.Form) // print form information in server side
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
-	fmt.Fprintf(w, "Hello astaxie!") // send data to client side
-}
-
 func main() {
-	//http.HandleFunc("/", sayhelloName)     // set router
-	http.Handle("/", http.FileServer(http.Dir("/../leaftut")))
+	fmt.Println("started. gonna run wwwserver:")
+	http.Handle("/", http.FileServer(http.Dir("leaftut/")))
 	err := http.ListenAndServe(":80", nil) // set listen port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
